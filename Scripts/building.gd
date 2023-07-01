c2aefa73377d3817329e18bd404e3f5a032adbf5
Kix4Christ <kix11@icloud.com>
2023-07-01 19:32:18 -0400

    made building better

--- Scripts/building.gd
+++ Scripts/building.gd
@@ -3,7 +3,9 @@ extends StaticBody2D
 signal player_entered
 signal player_exited
 
-
+func _ready():
+	$AnimatedSprite2D.play("default")
+	
 func _on_proximity_body_entered(body):
 	
 	if body.has_method("is_player"):
--- Scripts/gold_piece.gd
+++ Scripts/gold_piece.gd
@@ -2,38 +2,10 @@ extends Area2D
 
 @export var value = 1
 
-@export var original_velocity = -50
-@export var bounce_acceleration = 50
-
-var bouncing = false
-var bounce_height = 0
-var bounce_velocity = original_velocity
-var original_y
 
 func _ready():#go to original position
-	original_y = randi_range(0, 32*8)
-	position = Vector2(randi_range(0, 32*12), original_y)
+	position = Vector2(randi_range(0, 32*12), randi_range(0, 32*8))
 	#$Bounce_Timer.start()
-
-func _process(delta):
-	
-	if bouncing:#do all of the fancy movment things
-		position.y = original_y + (bounce_velocity * delta)
-		bounce_velocity += bounce_acceleration * delta
-		
-		if position.y < original_y:
-			position.y = original_y
-			bounce_velocity = original_velocity
-			$Bounce_Timer.start()
-			bouncing = false
-
-
-
-func _on_bounce_timer_timeout():#start bouncing and set a new wait time for later
-	bouncing = true
-	$Bounce_Timer.wait_time = randi_range(5, 10)
-	print("boing")
-
 
 
 
@@ -41,3 +13,5 @@ func _on_body_entered(body):
 	if body.has_method("is_player"):
 		Global.player_gold += value
 		queue_free()
+	else:
+		_ready()
